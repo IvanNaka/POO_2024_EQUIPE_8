3c833e5815f3d897e1ade9d020c3290f410c34f6
Ivan Nakatani <ivan.nakatani@gmail.com>
2024-02-27 01:47:05 -0300

    PBL01 comentarios

--- src/PBL01.java
+++ src/PBL01.java
@@ -7,6 +7,7 @@ public class PBL01 {
         while (continuar){
             try{
                 Scanner input = new Scanner(System.in);
+                // Recebe os numeros inputados pelo usuario
                 System.out.println("Digite o primeiro número: ");
                 double numero1 = input.nextInt();
                 System.out.println("Digite o segundo número: ");
@@ -16,7 +17,9 @@ public class PBL01 {
                 System.out.println("-");
                 System.out.println("/");
                 System.out.println("*");
+                //Recebe a operacao desejada pelo usuario
                 String operacao = input.next();
+                //Realiza a operacao baseada no input
                 switch (operacao){
                     case "+":
                         numero1 += numero2;
@@ -31,19 +34,25 @@ public class PBL01 {
                         numero1 *= numero2;
                         break;
                     default:
+                        // Caso o input nao tenha sido mapeado estoura uma excecao
                         throw new InvalidParameterException();
                 }
+                // Mostra o resultado da operacao
                 System.out.println("O resultado é: " + numero1);
+                //Verifica se o usuario deseja ou nao continuar
                 System.out.println("Você deseja continuar?(S/N)");
                 String continuarString = input.next();
                 if (continuarString.equalsIgnoreCase("N")){
                     continuar = false;
                 }
             }catch (InputMismatchException e){
+                //Tratamento de excecao caso o input de numero seja invalido
                 System.out.println("Insira um número válido");
             }catch (InvalidParameterException e){
+                //Tratamento de excecao caso o input de operacao seja invalido
                 System.out.println("Insira uma operação válida");
             }catch (Exception e) {
+                //Tratamento de excecao caso aconteca algum erro nao mapeado
                 System.out.println("Erro ao realizar conta");
             }
         }
