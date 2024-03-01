38b40c7acf5697b8811b74c0bb222034fe00250d
Ivan Nakatani <ivan.nakatani@gmail.com>
2024-03-01 18:26:47 -0300

    PBL02 arquivo

---
+++ src/PBL02.java
@@ -0,0 +1,9 @@
+import java.security.InvalidParameterException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PBL02 {
+    public  static void main (String[] args){
+
+    }
+}
